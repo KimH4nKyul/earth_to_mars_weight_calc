7cb73940f903bed06b96334c6db028034af10403
KimH4nKyul <alskim93@kakao.com>
2023-02-15 22:35:37 +0900

    [Add] cacluate weight on mars.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight/9.81) * 3.711
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut mars_weight: f32 = calculate_weight_on_mars(64.7);
+    mars_weight = mars_weight * 1000.0;
+    println!("Ur weight on mars: {}g", mars_weight);
 }
