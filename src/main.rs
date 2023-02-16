ef78a9da174ca98847d6a1efa449fbb85427ff04
KimH4nKyul <alskim93@kakao.com>
2023-02-16 15:24:27 +0900

    [Update] finished project.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
+use std::io;
+
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight/9.81) * 3.711
 }
 
 fn main() {
-    let mut mars_weight: f32 = calculate_weight_on_mars(64.7);
-    mars_weight = mars_weight * 1000.0;
-    println!("Ur weight on mars: {}g", mars_weight);
+    println!("당신의 몸무게를 입력하세요: ");
+    let mut input = String::new();   
+    io::stdin().read_line(&mut input).unwrap();
+    println!("Input: {}", input);
+
+    // let w: f32 = input.trim().parse().unwrap();
+    let w = input.trim();
+    let real_w = w.parse::<f32>().unwrap();
+
+    let mars_weight: f32 = calculate_weight_on_mars(real_w);
+    println!("Ur weight on mars: {}kg", mars_weight);
 }
